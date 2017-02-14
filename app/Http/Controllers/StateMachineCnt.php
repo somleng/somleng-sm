dfa0a5df8668bb7d920b975d592150cd26108999
phyrum <phyrum@open.org.kh>
2017-02-14 14:04:57 +0700

    log ->call_Sid

--- app/Http/Controllers/StateMachineCnt.php
+++ app/Http/Controllers/StateMachineCnt.php
@@ -177,8 +177,9 @@ class StateMachineCnt extends Controller
      */
     public function sm_callflow(Request $request)
     {
-        Log::debug($request);
+
         $this->call_Sid = $request->CallSid;
+        Log::info("callSid= " .  $this->call_Sid);
 //        dd($this->call_Sid);
 
         $getStates = $this->tbl_states->getStatesFromStateTable('1');
