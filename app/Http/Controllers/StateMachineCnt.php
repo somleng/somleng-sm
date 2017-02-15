bedac657d3e85a61bccaecd975ed9d7446e62dd2
Samak <csamak@open.org.kh>
2017-02-15 10:20:45 +0700

    debug return value from validation function using request

--- app/Http/Controllers/StateMachineCnt.php
+++ app/Http/Controllers/StateMachineCnt.php
@@ -181,9 +181,10 @@ class StateMachineCnt extends Controller
 //        dd($this->call_Sid);
         /*if(!empty($request->return_input))
         {*/
+            Log::info("return_input using request input= " .  $request->input('return_input'));
 //            Log::info("return_input = " .  $request->return_input);
 //            Log::info("return_input using REQUEST= " .  $_REQUEST['return_input']);
-            Log::info("return_input using input= " .  Input::get('return_input'));
+//            Log::info("return_input using input= " .  Input::post('return_input'));
 //            Log::info("return_input using POST= " .  $_POST['return_input']);
 //        }
 
