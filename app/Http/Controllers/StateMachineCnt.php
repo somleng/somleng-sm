e1418929e65e3d78e64d3b0bad74c4ee2cf408d5
phyrum <phyrum@open.org.kh>
2017-02-15 16:13:35 +0700

    testing wrong input from E0 to B

--- app/Http/Controllers/StateMachineCnt.php
+++ app/Http/Controllers/StateMachineCnt.php
@@ -347,7 +347,6 @@ class StateMachineCnt extends Controller
                     ),
                     array(
                         'to' => array('E0'), 'do' => function($current_state) {
-//                        Log::debug($current_state);
                         $this->changeState($this->call_Sid, $current_state);
                     }
                     ),
@@ -396,12 +395,12 @@ class StateMachineCnt extends Controller
             if($this->return_input != 0)
             {
                 $stateMachine->apply($transition[1]);
-//                Log::debug($transition[1]);
+                Log::debug($transition[1]);
             }
             else // when $return_input = 0 || null
             {
                 $stateMachine->apply($transition[0]);
-//                Log::debug($transition[0]);
+                Log::debug($transition[0]);
             }
 
         }
