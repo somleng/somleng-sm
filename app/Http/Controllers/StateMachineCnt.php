5f8c5d185becb564844d907788b38cc4a2e34e60
phyrum <phyrum@open.org.kh>
2017-03-02 14:38:28 +0700

    test callid in statemachinecontroller and sendrequesttosomleng whether it is equal or not

--- app/Http/Controllers/StateMachineCnt.php
+++ app/Http/Controllers/StateMachineCnt.php
@@ -187,7 +187,7 @@ class StateMachineCnt extends Controller
         Log::info('2=' . $request->CallSid);
         $tbl_twiml_after_queue = new tbltwimlafterqueue;
         $result = $tbl_twiml_after_queue->getTwilmlText($request->CallSid);
-        Log::info('result=' . $result);
+        Log::info('result=' . str($result));
         return $result;
 
 //        $content1 = "test ";
